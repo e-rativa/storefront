fc912ee408f963ac4adaa394e8252bcea5b14b81
oscar cortes <oe.cortes@uniandes.edu.co>
2023-03-07 22:33:31 -0500

    inventarios

--- ms-intentarios/app/broker/controllers/command_controller.py
+++ ms-intentarios/app/broker/controllers/command_controller.py
@@ -11,19 +11,6 @@ dispatcher = Dispatcher()
 
 class CommandController:
 
-    def OrderCommandCreator(self, data):
-        topico = 'order-command-create'
-        payload = CreateOrderPayload(
-            product_uuid=str(data['product_uuid']),
-            product_quantity=str(data['product_quantity']),
-            order_type=str(data['order_type']),
-            address=str(data['address'])
-        )
-        
-        comando_integracion = CommandCreateOrder(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topico, AvroSchema(CommandCreateOrder))
-
-
     def StockCommandValidator(self, data):
         topic = 'product-command-validateStock'
         payload = StockValidaterPayload(
@@ -34,20 +21,6 @@ class CommandController:
         
         comando_integracion = CommandStockValidate(data=payload)
         dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(CommandStockValidate))
-
-
-    def RouteCommandCreate(self, data):
-        topic = 'route-command-create'
-        payload = CreateRoutePayload(
-            order_uuid=str(data['order_uuid']),
-            product_uuid=str(data['product_uuid']),
-            product_quantity=str(data['product_quantity']),            
-            address=str(data['address']),            
-        )
-        
-        comando_integracion = CreateRouteValidate(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(CreateRouteValidate))
-
 
     def ProductCommandPrepare(self, data):
         topic = 'product-command-prepareProduct'
--- ms-intentarios/app/broker/controllers/event_controller.py
+++ ms-intentarios/app/broker/controllers/event_controller.py
@@ -14,23 +14,6 @@ dispatcher = Dispatcher()
 
 class EventController:
 
-    def OrderCompletedEvent(self, data):
-        topic = 'order-event-completed'
-        payload = OrderCompletedPayload(
-            order_uuid=str(data['order_uuid'])            
-        )
-        comando_integracion = EventOrderCompleted(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventOrderCompleted))
-
-
-    def OrderStartedEvent(self, data):
-        topic = 'order-event-started'
-        payload = OrderStartedPayload(
-            order_uuid=str(data['order_uuid'])            
-        )
-        comando_integracion = EventOrderStarted(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventOrderStarted))
-
     def ProductAvailableEvent(self, data):
         topic = 'product-event-available'
         payload = ProductAvailablePayload(
@@ -50,26 +33,3 @@ class EventController:
         )
         comando_integracion = EventProductUnavailable(data=payload)
         dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventProductUnavailable))
-
-    def RouteCreatedEvent(self, data):
-        topic = 'route-event-created'
-        payload = RouteCreatedPayload(
-            route_uuid = str(data['route_uuid']),
-            order_uuid = str(data['order_uuid']),
-            product_uuid = str(data['product_uuid']),
-            product_quantity = str(data['product_quantity']),
-            address = str(data['address'])           
-        )
-        comando_integracion = EventRouteCreated(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventRouteCreated))
-
-    def RouteUnavailableEvent(self, data):
-        topic = 'route-event-unavailable'
-        payload = RouteUnavailablePayload(
-            order_uuid = str(data['order_uuid']),
-            product_uuid = str(data['product_uuid']),
-            product_quantity = str(data['product_quantity']),
-            address = str(data['address']),          
-        )
-        comando_integracion = EventRouteUnavailable(data=payload)
-        dispatcher._publicar_mensaje(comando_integracion, topic, AvroSchema(EventRouteUnavailable))
